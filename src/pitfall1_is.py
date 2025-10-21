5adfa52b7031c0a9744d1b568c15ec2a7124fda9
Stefan Ivanetic <stefan.ivanetic@gmail.com>
2025-10-21 21:26:23 +0200

    Use rich

--- src/pitfall1_is.py
+++ src/pitfall1_is.py
@@ -1,21 +1,20 @@
+from rich import print as rprint
+
 while True:
     entry = input("Enter a number, or 'q' to quit: ")
 
     if entry == "q":
         break
 
-    if not entry.isdigit():
-        raise ValueError("That's not a valid number!")
-
     number = int(entry)
-    print("=" * 40)
-    i = 0
+    rprint("=" * 40)
+    i = -10
     while True:
         if i is number:
-            print(f"Found it! i is {i}")
+            rprint(f"Found it! i is {i}")
             break
         i += 1
 
         if i > 100_000:
-            print("Giving up after 100,000 iterations")
+            rprint("Giving up after 100,000 iterations")
             break
